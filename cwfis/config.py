45cd4892fbb0599a47109433a0d1de35c2d05cad
Ann Margaret Silva <141737779+annsilva@users.noreply.github.com>
2024-06-04 01:39:57 +0000

    convert to bson and resolve connection errors

--- cwfis/config.py
+++ cwfis/config.py
@@ -2,8 +2,8 @@
 # base_url = input("Enter the base URL: ") #https://cwfis.cfs.nrcan.gc.ca/downloads/hotspots/archive/
 base_url ='https://cwfis.cfs.nrcan.gc.ca/downloads/hotspots/archive/'
 extract_to = 'extracted_data'
-db_name = 'local'
-collection_name = 'wildfire'
+db_name = 'wildfire_db'
+collection_name = 'wildfire_collection'
 mongo_uri = 'mongodb+srv://wildfiremongodb:passwordatlas@wildfirecluster.vmiph7u.mongodb.net/mongo'
 # start_year = int(input("Enter start year: "))
 # end_year = int(input("Enter end year: "))
